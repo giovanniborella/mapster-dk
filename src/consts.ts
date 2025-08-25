70f5c281c2b231ecb1bd41aeef69c3a26854330d
René Giovanni Borella <rgb@mapster.dk>
2025-08-25 08:06:14 +0200

    clean root structure

--- src/consts.ts
+++ src/consts.ts
@@ -3,29 +3,17 @@ import type { Site, Metadata, Socials } from "@types";
 export const SITE: Site = {
   NAME: "Mapster.dk",
   EMAIL: "rgb@mapster.dk",
-  NUM_POSTS_ON_HOMEPAGE: 3,
-  NUM_WORKS_ON_HOMEPAGE: 2,
-  NUM_PROJECTS_ON_HOMEPAGE: 3,
+  NUM_PROJECTS_ON_HOMEPAGE: 3
 };
 
 export const HOME: Metadata = {
   TITLE: "Home",
-  DESCRIPTION: "Astro Nano is a minimal and lightweight blog and portfolio.",
-};
-
-export const BLOG: Metadata = {
-  TITLE: "Blog",
-  DESCRIPTION: "A collection of articles on topics I am passionate about.",
-};
-
-export const WORK: Metadata = {
-  TITLE: "Work",
-  DESCRIPTION: "Where I have worked and what I have done.",
+  DESCRIPTION: "Minimal and lightweight portfolio.",
 };
 
 export const PROJECTS: Metadata = {
   TITLE: "Projects",
-  DESCRIPTION: "A collection of my projects, with links to repositories and demos.",
+  DESCRIPTION: "A collection of my projects, physical and digital.",
 };
 
 export const SOCIALS: Socials = [
